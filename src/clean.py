cae741b1a3d15afa413b21b38b4820313319cd0e
BAYARD lea <bayard@informatique.univ-paris-diderot.fr>
2023-01-05 17:34:40 +0100

    xml avec titre et text dans page, sans majuscules et sans accents

--- src/clean.py
+++ src/clean.py
@@ -1,14 +1,25 @@
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
+import re #regex
 
-tree = et.parse('frwiki10000.xml')
+tree = ET.parse('data/frwiki10000.xml')
 root = tree.getroot()
 
-def printTitle():
-    for child in root:
-        for c in child:
-            if c.tag.endswith('title'):
-                print(c.text)
+def TOFIL(str):
+    str = str.lower()
+    sans_accents = {'é':'e', 'è':'e', 'ê':'e', 'ë':'e', 'ô':'o', 'ö':'o', 'à':'a', 'â':'a', 'ä':'a', 'ù':'u', 'ü':'u', 'û':'u', 'î':'i', 'ï':'i', 'ç':'c'}
+    for k in sans_accents.keys():
+        str = str.replace(k, sans_accents[k])
+    return str
 
-for child in root:
-    for c in child:
-        print(c)
+pages = ET.Element('pages')
+
+for page in root[1:]:
+    p = ET.SubElement(pages, 'page')
+    titre = ET.SubElement(p, 'titre')
+    titre.text = TOFIL(page[0].text)
+    for elem in page[3]:
+        if elem.tag.endswith('text'):
+            text = ET.SubElement(p, 'text')
+            text.text = TOFIL(elem.text)
+
+print(ET.tostring(pages, encoding='utf8', method='xml'))
