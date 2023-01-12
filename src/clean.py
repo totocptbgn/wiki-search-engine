0816c4aa7866b5537fd2caf60b8b08b6d9348825
Thomas Bignon <35891830+totocptbgn@users.noreply.github.com>
2023-01-12 14:10:50 +0100

    Added lib

--- src/clean.py
+++ src/clean.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import mwparserfromhell
 import re
 
 # Script qui prends comme entrée un fichier Wikimédia Dump, qui extrait et traite les données nécessaire, puis les écrit dans un XML.
@@ -23,6 +24,8 @@ def string_treatment(str):
 def text_treatment(str):
 
     # On enlève les balises ref et leurs contenus
+    str = str.replace('&lt;', '<')
+    str = str.replace('&gt;', '>')
     str = re.sub(r'<ref>([\s\S]*?)</ref>', '', str)
 
     # On enlève du gras et des italiques
@@ -37,18 +40,27 @@ def text_treatment(str):
 
     return str
 
-# Pour chaque page, extraire le titre et le textz, les traiter puis les insérer dans un nouvel élément XML
+# Pour chaque page, extraire le titre et le texte, les traiter puis les insérer dans un nouvel élément XML
 root = tree.getroot()
-pages = ET.Element('pages')
+#pages = ET.Element('pages')
 for page in root[1:]:
-    p = ET.SubElement(pages, 'page')
-    titre = ET.SubElement(p, 'titre')
-    titre.text = string_treatment(page[0].text)
+    #p = ET.SubElement(pages, 'page')
+    #titre = ET.SubElement(p, 'titre')
+    #titre.text = string_treatment(page[0].text)
     for elem in page[3]:
         if elem.tag.endswith('text'):
-            text = ET.SubElement(p, 'text')
-            text.text = text_treatment(string_treatment(elem.text))
+            
+            #text = ET.SubElement(p, 'text')
+            #text.text = text_treatment(string_treatment(elem.text))
+            wiki_array = mwparserfromhell.parse(elem.text).filter_text()
+            for word in wiki_array:
+                # print(re.split(r'\W+', str(word)), str(word))
+                print(str(word))
+            words = []
+    print(page[0].text)
+    break
+
 
 # On écrit le fichier xml 
-output_root = ET.ElementTree(pages)
-output_root.write('out.xml')
+# output_root = ET.ElementTree(pages)
+# output_root.write('out.xml')
