210a7ed1bb079304a6791249c74c47ab0cc4569a
totocptbgn <thomas.coptbignon@gmail.com>
2023-02-07 23:11:19 +0000

    fix

--- src/collector.py
+++ src/collector.py
@@ -31,22 +31,22 @@ sorted_occur = [(k, v) for k, v in sorted(occur.items(), key=lambda item: item[1
 if True:
     print(f'Le corpus est composé de {len(sorted_occur)} mots différents.')
     print("Voici les mots les plus communs :")
-    for i in sorted_occur[:48]:
+    for i in sorted_occur[:50]:
         print(i[0], " " * (15 - len(i[0])), i[1])
 
 # On garde les 20000 mots qui apparaissent le plus
-sorted_occur_crop = sorted_occur[:20000]
+# sorted_occur_crop = sorted_occur[:20000]
 
 # On la tri par ordre alphabétique
-sorted_occur_crop = sorted(sorted_occur_crop, key=lambda x: x[0])
+# sorted_occur_crop = sorted(sorted_occur_crop, key=lambda x: x[0])
 
 # Fonction qui prends en paramêtre un mot s "mal écrit" et le compare à une liste de mots words pour retrouver le mot le plus proche
-def closest_word(s, words):
-    return words[np.argmax([SequenceMatcher(None, s, w[0]).ratio() for w in sorted_occur])][0]
+# def closest_word(s, words):
+#    return words[np.argmax([SequenceMatcher(None, s, w[0]).ratio() for w in sorted_occur])][0]
 
 ## Exercice 3
 
 # On calcule le coefficient IDF de chaque mot
 # IDF = dict()
 # for word, page_array in word_page_relationships.items():
-    IDF[word] = np.log10(page_count / len(page_array))
+    # IDF[word] = np.log10(page_count / len(page_array))
