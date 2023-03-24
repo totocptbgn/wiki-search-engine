1e94d6513feefb3f643f6134c8541be10743c857
Thomas Copt-Bignon <35891830+totocptbgn@users.noreply.github.com>
2023-03-24 14:43:10 +0100

    Update lemmatisationParse.py

--- src/RecupTitles/lemmatisationParse.py
+++ src/RecupTitles/lemmatisationParse.py
@@ -2,80 +2,62 @@ import xml.etree.ElementTree as ET
 import mwparserfromhell
 import re
 from tqdm import tqdm
-# pip install spacy
-# python -m spacy download fr_core_news_sm
 import spacy
-nlp = spacy.load("fr_core_news_sm")
 from nltk.stem.snowball import SnowballStemmer
 import sys
 
-if (len(sys.argv) != 2):
-    print("Waited: python " + sys.argv[0] + " [input file]")
-    exit()
+# pip install spacy
+# python -m spacy download fr_core_news_sm
 
-french_stopwords = open('out.txt')
-sw_list = french_stopwords.read().splitlines()
+spacy.require_gpu()
+nlp = spacy.load("fr_core_news_sm", disable=["tagger", "parser", "ner", "textcat"])
+nlp.max_length = 10000000
 
+if len(sys.argv) != 2:
+    print(f"Usage: python {sys.argv[0]} [input file]")
+    sys.exit(1)
 
-# Script qui prends comme entrée un fichier Wikimédia Dump, qui extrait et traite les données nécessaire, puis les écrit dans un XML.
+with open('out.txt') as f:
+    sw_list = f.read().splitlines()
 
-# Lecture du fichier
-filename = sys.argv[1]
-
-# Traitement des strings
-def string_treatment(str):
-
-    # et on lemmatise
-    doc = nlp(str)
+def string_treatment(string):
+    doc = nlp(string)
     words = [token.lemma_ for token in doc if token.text not in sw_list]
+    return ' '.join(words)
 
-    str = ' '.join(words)
+def main():
+    input_file = sys.argv[1]
+    page = None
+    nb_pages = 0
 
-    return str
+    with open('corpusLemm.xml', 'w') as f:
+        f.write('<pages>')
 
-page = None
-titre = None
-text = None
-links = None
-nbPages = 0
+    for event, elem in tqdm(ET.iterparse(input_file, events=("start", "end"))):
+        if event == 'start':
+            if elem.tag == 'page':
+                page = ET.Element('page')
+                nb_pages += 1
+            elif elem.tag == 'title':
+                title = ET.SubElement(page, 'title')
+            elif elem.tag == 'text':
+                text = ET.SubElement(page, 'text')
+            elif elem.tag == 'links':
+                links = ET.SubElement(page, 'links')
+        elif event == 'end':
+            if elem.tag == 'title':
+                title.text = elem.text
+                if nb_pages % 1000 == 0:
+                    print(f"{nb_pages} :   {title.text}")
+            elif elem.tag == 'text':
+                text.text = string_treatment(elem.text)
+            elif elem.tag == 'links':
+                links.text = elem.text
+                with open('corpusLemm.xml', 'ab') as f:
+                    f.write(ET.tostring(page, encoding='utf-8', method='xml'))
+    
+    with open('corpusLemm.xml', 'a') as f:
+        f.write('</pages>')
 
-with open('corpusLemm.xml', 'w') as f:
-    f.write('<pages>')
-
-# Pour chaque page, extraire le titre et le texte, les traiter puis les insérer dans un nouvel élément XML
-for event, elem in ET.iterparse(filename, events=("start", "end")):
-
-    if event == 'start':
-
-        if elem.tag == 'page':
-            page = ET.Element('page')
-            nbPages += 1
-
-        elif elem.tag == 'title':
-            titre = ET.SubElement(page, 'title')
-
-        elif elem.tag == 'text':
-            text = ET.SubElement(page, 'text')
-
-        elif elem.tag == 'links':
-            links = ET.SubElement(page, 'links')
-
-    if event == 'end':
-
-        if elem.tag == 'title':
-            titre.text = elem.text
-
-            if nbPages % 1000 == 0:
-                print(str(nbPages) + " :   " + titre.text)
-
-        elif elem.tag == 'text':
-            text.text = string_treatment(elem.text)
-
-        elif elem.tag == 'links':
-            links.text = elem.text
-
-            with open('corpusLemm.xml', 'a') as f:
-                f.write(ET.tostring(page, encoding='unicode', method='xml'))
-
-with open('corpusLemm.xml', 'a') as f:
-    f.write('</pages>')
+if __name__ == '__main__':
+    main()
