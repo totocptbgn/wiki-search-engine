21a900be23371d8db58637e1b5d14c6dbacab32a
BAYARD lea <bayard@informatique.univ-paris-diderot.fr>
2023-03-24 17:34:54 +0100

    collector plus rapide ?

--- src/collector.py
+++ src/collector.py
@@ -1,12 +1,11 @@
 import xml.etree.ElementTree as ET
-from difflib import SequenceMatcher
-import numpy as np
 from tqdm import tqdm
 import psutil
 import os
 import pickle
 import sys
 import math
+from collections import Counter
 
 def printerr(str=None):
     print("Waited: python " + sys.argv[0] + " [input file] [output file idf] [output file word page relationship] [minimum tf-idf gardé] [nombre mots max gardés]")
@@ -38,24 +37,19 @@ print(f"début du calcul des {nbWords} mots les plus fréquents et init idf")
 
 occur = dict()                          # Dictionnaire : clé = mot, value = nombre d'occurence dans tout les documents
 page_count = 0                          # L'identifiant de la page traitée
-idf = dict()
+inPages = dict()
 
 for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
     if event == 'end' and elem.tag == 'text':
-        page = dict()
         words = elem.text.split()
-        for w in words:
-            if w in page:
-                page[w] += 1
-            else:
-                page[w] = 1
+        page = Counter(words)
         for w in page:
             if w in occur:
                 occur[w] += page[w]
-                idf[w] += 1
+                inPages[w] += 1
             else:
                 occur[w] = page[w]
-                idf[w] = 1
+                inPages[w] = 1
         page_count += 1
         if page_count % 10000 == 0:
             print("Memory usage: {:.2f} MB".format(psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)))
@@ -67,11 +61,12 @@ del occur
 print(f"fin du calcul des {nbWords} mots les plus fréquents et init idf")
 print("début du calcul des idf")
 
-for w in tqdm(list(idf)):
-    if w not in most_commons:
-        del idf[w]
-    else:
-        idf[w] = math.log10(page_count / idf[w])
+idfAll = dict()
+
+for w in tqdm(most_commons):
+    idfAll[w] = math.log10(page_count / inPages[w])
+
+del inPages
 
 print("calcul des idf fini")
 print("début du calcul des relations mot-page")
@@ -82,20 +77,15 @@ word_page_relationships = dict()
 
 for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
     if event == 'end' and elem.tag == 'text':
-        tf = dict()
-        words = elem.text.split()
-        for w in words:
-            if w in tf:
-                tf[w] += 1
-            elif w in most_commons:
-                tf[w] = 1
+        words = [w for w in elem.text.split() if w in most_commons]
+        tf = Counter(words)
         for w in tf:
             tf[w] = 1 + math.log10(tf[w])
         Nd = math.sqrt(sum([t**2 for t in tf.values()]))
         for w in tf:
             tf[w] /= Nd
         for w in tf:
-            if tf[w] * idf[w] >= minTF_IDF:
+            if tf[w] * idfAll[w] >= minTF_IDF:
                 if w in word_page_relationships:
                     word_page_relationships[w].append((page_count, tf[w]))
                 else:
@@ -107,9 +97,12 @@ for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
 print("calcul des relations mot-page fini")
 print("début suppression des idf non utilisés")
 
-for w in tqdm(list(idf)):
-    if w not in word_page_relationships:
-        del idf[w]
+idf = dict()
+
+for w in tqdm(word_page_relationships):
+    idf[w] = idfAll[w]
+
+del idfAll
 
 print("fin suppression des idf non utilisés")
 
