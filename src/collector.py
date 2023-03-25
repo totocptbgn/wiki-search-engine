7f65c12555726121f101d6f808bf6d9ac00e9614
BAYARD lea <bayard@informatique.univ-paris-diderot.fr>
2023-03-25 15:15:08 +0100

    vitesse

--- src/collector.py
+++ src/collector.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 from tqdm import tqdm
 import psutil
 import os
 import pickle
-import sys
 import math
 from collections import Counter
 import re
@@ -11,13 +10,15 @@ import multiprocessing
 from multiprocessing import Pool
 import numpy as np
 from queue import Queue
+import argparse
+import sys
 
 def printerr(str=None):
     print("Waited: python " + sys.argv[0] + " [input file] [output file idf] [output file word page relationship] [minimum tf-idf gardé] [nombre mots max gardés]")
     if str != None:
         print(str)
 
-def processPage(text, most_commons, idf, minTF_IDF, page_count, word_regex):
+def processPageDict(text, most_commons, idf, minTF_IDF, page_count, word_regex):
     words = [w for w in word_regex.findall(text) if w in most_commons]
     tf = Counter(words)
     for w in tf:
@@ -35,33 +36,39 @@ def processPage(text, most_commons, idf, minTF_IDF, page_count, word_regex):
     return result
 
 if __name__ == '__main__':
-    affPlaceDiv = 1024 * 1024
-    mem_info = psutil.Process(os.getpid())
 
     if (len(sys.argv) != 6):
         printerr()
         exit()
 
-    minTF_IDF = float(sys.argv[4])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file')
+    parser.add_argument('idf_file')
+    parser.add_argument('word_page_file')
+    parser.add_argument('minTF_IDF')
+    parser.add_argument('nb_words')
+    args = parser.parse_args()
+
+    affPlaceDiv = 1024 * 1024
+    mem_info = psutil.Process(os.getpid())
+
+    minTF_IDF = float(args.minTF_IDF)
 
     if minTF_IDF < 0. or minTF_IDF >= 1.:
         printerr("Le minimum de TF-IDF à garder doit être un flottant compris entre 0 inclus et 1 exclu")
         exit()
 
-    nbWords = int(sys.argv[5])
+    nbWords = int(args.nb_words)
 
     if nbWords < 15000:
         printerr("Minimum de mots à garder: 15000")
         exit()
 
-    idf_file = open(sys.argv[2], 'wb')
-    word_page_file = open(sys.argv[3], 'wb')
+    idf_file = open(args.idf_file, 'wb')
+    word_page_file = open(args.word_page_file, 'wb')
 
     word_regex = re.compile(r'\b\w+\b')
-    num_processes = multiprocessing.cpu_count()
-
-    if num_processes > 1:
-        num_processes //= 2
+    num_processes = multiprocessing.cpu_count() - 2
 
     page_count = 0
 
@@ -72,8 +79,8 @@ if __name__ == '__main__':
     inPages = dict()
     text = []
 
-    for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
-        if event == 'end' and elem.tag == 'text':
+    for event, elem in tqdm(etree.iterparse(args.input_file, events=("end",))):
+        if elem.tag == 'text':
             words = word_regex.findall(elem.text)
             page = Counter(words)
             for w in page:
@@ -85,7 +92,7 @@ if __name__ == '__main__':
                     inPages[w] = 1
                 page_count += 1
             if page_count % 10000 == 0:
-                print("Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
+                print(str(page_count) + "Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
 
     most_commons = [k for k, v in sorted(occur.items(), key=lambda item: item[1], reverse=True)][:nbWords]
 
@@ -110,12 +117,12 @@ if __name__ == '__main__':
 
     with Pool(processes=num_processes) as pool:
         results = Queue()
-        for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
-            if event == 'end' and elem.tag == 'text':
-                results.get(pool.apply_async(processPage, args=(elem.text, most_commons, idfAll, minTF_IDF, page_count, word_regex)))
+        for event, elem in tqdm(etree.iterparse(args.input_file, events=("end",))):
+            if elem.tag == 'text':
+                results.get(pool.apply_async(processPageDict, args=(elem.text, most_commons, idfAll, minTF_IDF, page_count, word_regex)))
                 page_count += 1
                 if page_count % 10000 == 0:
-                    print("Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
+                    print(str(page_count) + "Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
 
         print("sous-dicos ok")
         print("nombre de résultats:", len(results))
@@ -133,27 +140,7 @@ if __name__ == '__main__':
             nb_add += 1
             if nb_add % 1000 == 0:
                 print("nombre pages traitées:", nb_add)
-                print("Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
-
-
-    #for event, elem in tqdm(ET.iterparse(sys.argv[1], events=("start", "end"))):
-    #    if event == 'end' and elem.tag == 'text':
-    #        words = [w for w in word_regex.findall(elem.text) if w in most_commons]
-    #        tf = Counter(words)
-    #        for w in tf:
-    #            tf[w] = 1 + math.log10(tf[w])
-    #        Nd = math.sqrt(sum([t**2 for t in tf.values()]))
-    #        for w in tf:
-    #            tf[w] /= Nd
-    #        for w in tf:
-    #            if tf[w] * idfAll[w] >= minTF_IDF:
-    #                if w in word_page_relationships:
-    #                    word_page_relationships[w].append((page_count, tf[w]))
-    #                else:
-    #                    word_page_relationships[w] = [(page_count, tf[w])]
-    #        page_count += 1
-    #        if page_count % 10000 == 0:
-    #            print("Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
+                print(str(nb_add) + "Memory usage: {:.2f} MB".format(mem_info.memory_info().rss / affPlaceDiv))
 
     print("calcul des relations mot-page fini")
     print("début suppression des idf non utilisés")
