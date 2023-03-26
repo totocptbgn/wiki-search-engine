a0a72c56b5e7887d39e02fe8a4e2369e176a2d53
Thomas Copt-Bignon <thomas.coptbignon@gmail.com>
2023-03-26 21:53:41 +0200

    even better server

--- src/app.py
+++ src/app.py
@@ -1,25 +1,27 @@
 from flask import Flask, render_template, request
 from score import bestPages
+import urllib.parse
 
 app = Flask(__name__)
 
+def title_to_link(title):
+    return  "https://fr.wikipedia.org/wiki/" + title.replace(" ", "_")
+    
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    search_results = []
+    results = []
     alpha = 0.5
     gamma = 0.5
 
     if request.method == "POST":
-        print("request.form")
-        print(request.form)
         user_input = request.form.get("search_text")
         alpha = float(request.form.get("alpha", 0.5))
         gamma = float(request.form.get("gamma", 0.5))
-
-        print("debug toto", user_input, alpha, gamma)
         search_results = bestPages(alpha, gamma, user_input)
+        results = [(sr, title_to_link(sr)) for sr in search_results]
 
-    return render_template("index.html", search_results=search_results, alpha=alpha, gamma=gamma)
+    return render_template("index.html", search_results=results, alpha=alpha, gamma=gamma)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
