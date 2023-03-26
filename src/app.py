84f2eb27fb37b57eb211ed270e89bbd2caf32bb0
Thomas Copt-Bignon <thomas.coptbignon@gmail.com>
2023-03-26 22:34:18 +0200

    even better server

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ import urllib.parse
 app = Flask(__name__)
 
 def title_to_link(title):
-    return  "https://fr.wikipedia.org/wiki/" + title.replace(" ", "_")
-    
+    return "https://fr.wikipedia.org/wiki/" + title.replace(" ", "_")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -19,7 +19,10 @@ def index():
         alpha = float(request.form.get("alpha", 0.5))
         gamma = float(request.form.get("gamma", 0.5))
         search_results = bestPages(alpha, gamma, user_input)
-        results = [(sr, title_to_link(sr)) for sr in search_results]
+        results = [f"<p><a href=\"{title_to_link(sr)}\" target=\"_blank\" rel=\"noopener noreferrer\">{sr}</a></p>" for sr in search_results]
+
+        if len(results) == 0:
+            results = ["<p style=\"margin-top: 0px;margin-bottom: 0px;\">Aucun résultat trouvé...</p>"]
 
     return render_template("index.html", search_results=results, alpha=alpha, gamma=gamma)
 
