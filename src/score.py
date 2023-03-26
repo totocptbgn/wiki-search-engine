e1254248b439cee77909fd82d2cfd563defd52f3
BAYARD lea <bayard@informatique.univ-paris-diderot.fr>
2023-03-26 12:47:33 +0200

    debug score

--- src/score.py
+++ src/score.py
@@ -17,7 +17,7 @@ with open("../data/word_page.dict", 'rb') as word_page_file:
 
 spec_regex = re.compile(r'[^a-z ]+')
 
-def requete(list):
+def common_pages(list):
     k=len(list)
     pointeur = [0]*k
     L = []
@@ -43,10 +43,10 @@ def anyEnd(list, pointeur):
 
 def score_frequence(idfs, tfs):
     Nr = math.sqrt(sum([idf**2 for idf in idfs]))
-    return sum([idfs[i] * tfs[i] for i in range(len(page))]) / Nr
+    return sum([idfs[i] * tfs[i] for i in range(len(idfs))]) / Nr
 
 def score(alpha, beta, gamma, idfs, tfs, pagerank):
-    return alpha * score_frequence(idfs) + beta * pagerank**gamma
+    return alpha * score_frequence(idfs, tfs) + beta * pagerank**gamma
 
 def scores(alpha, gamma, idfs, pages, pageranks):
     beta = 1. - alpha
@@ -60,12 +60,12 @@ def bestPages(alpha, gamma, requete):
     words = requete2words(requete)
     word_page = [word_page_relationships[w] for w in words]
     idfs = [idf[w] for w in words]
-    pages = requete(word_page)
+    pages = common_pages(word_page)
     return scores(alpha, gamma, idfs, pages, pageranks)
 
 def requete2words(requete):
     requete = requete.lower()
     requete = unicodedata.normalize('NFKD', requete)
-    requete = ''.join(c for c in str if not unicodedata.combining(c))
+    requete = ''.join(c for c in requete if not unicodedata.combining(c))
     requete = spec_regex.sub(r'', requete)
-    return [stemmer.stem[w] for w in requete.split()]
+    return [stemmer.stem(w) for w in requete.split()]
